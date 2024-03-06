9d2e7bf97c2adea2ec7c2f6af69bdc39cb7b4c0e
Joaquingro12 <jesus.guerrero@citi.com.mx>
2024-03-06 14:05:09 -0600

    Actualiando proyecto

--- integrales.py
+++
@@ -1,9 +0,0 @@
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-print("Estoy es otra prueba ni pedo" holaaaaa)
-=======
-print("Estoy es otra prueba ni pedo") de nuevo cambios
->>>>>>> Stashed changes
-=======
-print("Estoy es otra prueba ni pedo") de nuevo cambios
->>>>>>> Stashed changes
